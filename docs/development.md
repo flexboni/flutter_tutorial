c66af1cb68c3cbe2609894d2d7e097690bbf8082
flexboni <kgb8594@gmail.com>
2020-11-01 17:49:09 +0900

    Development - 'Using Material components' done

--- docs/development.md
+++ docs/development.md
@@ -1,6 +1,6 @@
 # Introduction to widgets
 
-## Basic widgets
+## [Basic widgets](https://flutter.dev/docs/development/ui/widgets-intro#basic-widgets)
 
 ### [Text](https://api.flutter.dev/flutter/widgets/Text-class.html)
 
@@ -18,7 +18,7 @@ Instead of being linearly oriented (either horizontally or vertically), a **Stac
 
 The Container widget lets you create a rectangular visual element. A container can be decorated with a BoxDecoration, such as a background, a border, or a shadow. A Container can also have margins, padding, and constraints applied to its size. In addition, a Container can be transformed in three dimensional space using a matrix.
 
-### For example
+### [For example](https://github.com/flexboni/flutter_tutorial/blob/master/lib/devmain1.dart) <- Click
 
 Below are some simple widgets that combine these and other widgets:
 
@@ -28,3 +28,20 @@ Dir : lib -> devmain1.dart
 
 If you wanna build the source, change 'devmain1.dart' to 'main.dart'
 
+</br>
+
+## [Using Material components](https://flutter.dev/docs/development/ui/widgets-intro#using-material-components)
+
+A Material app starts with the MaterialApp widget, which builds a number of useful widgets at the root of your app, including a Navigator, which manages a stack of widgets identified by strings, also known as “routes”. The Navigator lets you transition smoothly between screens of your application. 
+
+## [For example](https://github.com/flexboni/flutter_tutorial/blob/master/lib/devmain2.dart) <- Click
+
+Using the MaterialApp widget is entirely optional but a good practice.
+
+#### build
+
+Dir : lib -> devmain2.dart
+
+If you wanna build the source, change 'devmain2.dart' to 'main.dart'
+
+The Scaffold widget takes a number of different widgets as named arguments, each of which are placed in the Scaffold layout in the appropriate place
--- lib/main.dart
+++ lib/main.dart
@@ -2,62 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'My App',
-    home: MyScaffold(),
+    title: 'Flutter tutorial',
+    home: MyTutorialHome(),
   ));
 }
 
-class MyScaffold extends StatelessWidget {
+class MyTutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      // Column is a veritcal, linear layout,
-      child: Column(children: <Widget>[
-        MyAppBar(
-            title: Text('Example title',
-                style: Theme.of(context).primaryTextTheme.headline6)),
-        Expanded(
-            child: Center(
-          child: Text('Hello world!'),
-        ))
-      ]),
+    // 'Scaffold' is a layout for the major Material Component.
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation Menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+      // 'body' is the majority of the screen.
+      body: Center(
+        child: Text('Hello flutter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: 'Add',
+        child: Icon(Icons.add),
+      ),
     );
-  }
-}
-
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-
-  // Fieldes in a Widget subclass are always marked 'final'
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 56.0, // in logical pixel
-        // When you declare padding, use 'EdgeInsets'.
-        padding: const EdgeInsets.symmetric(
-            horizontal:
-                8.0), // symmetric() is set 'horizonal' or 'vertical' padding value
-        decoration: BoxDecoration(color: Colors.blue[500]),
-        // Row is a horizonal, linear layout.
-        child: Row(
-          // '<Widget>' is the type of items in the list.
-          children: <Widget>[
-            IconButton(
-                icon: Icon(Icons.menu),
-                tooltip: 'Navigation Menu',
-                onPressed: null),
-            // 'Expanded' expands its child to fill the available space.
-            Expanded(
-              child: title,
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            )
-          ],
-        ));
   }
 }
